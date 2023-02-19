d43fb2fa42b461b862afbadb78154da83424b3ce
Samarth Gupta <samarthgupta1202@gmail.com>
2023-02-19 16:57:41 +0530

    Site complete

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -2,33 +2,31 @@ import React from "react"
 import Navbar from "./components/Navbar.js"
 import Hero from "./components/Hero.js"
 import Card from "./components/Card.js"
-// import katie from "./components/katie.png"
-
-/*
-Challenge: Pass props to the Card component and display that data
-
-- img ("katie-zaferes.png")
-- rating ("5.0")
-- reviewCount (6)
-- country (Whatever you want)
-- title ("Life Lessons with Katie Zaferes")
-- price (136)
-
-*/
-
+import Data from "./Data.js"
 
 function App() {
+  const card = Data.map(element => {
+    return (
+      <Card
+        key={element.id}
+        {...element}
+      />
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+        {card}
+      </section>
 
-        <Navbar />
-        {/* <Hero /> */}
-        <Card img={"katie.png"} rating="5.0" reviewCount="(6)" country="Sweden" title="Life lessons with Katie Zaferes" price="136" />
-
-      </header>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+
--- airbnb/src/components/Card.js
+++ airbnb/src/components/Card.js
@@ -1,23 +1,34 @@
 import React from "react"
 
 function Card(props) {
+    console.log(props.element);
+
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    }
+    else if (props.country === "Online") {
+        badgeText = "Online"
+    }
+
+
+
     return (
         // <h1>This is a Card component</h1>
         <div className="card">
-            <img src={require(`./${props.img}`)} alt="katie" className="cardPhoto" />
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={require(`./${props.coverImg}`)} alt="katie" className="cardPhoto" />
 
             <div className="cardStats">
                 <img src={require("./star.png")} alt="star" className="starPhoto" />
-                <span>{props.rating}</span>
-                <span>{props.reviewCount} • </span>
-                <span> {props.country}</span>
+                <span>{props.stats.rating}</span>
+                <span>{props.stats.reviewCount} • </span>
+                <span>{props.country}</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price}</span> / person</p>
 
         </div>
-
-
 
     )
 }
